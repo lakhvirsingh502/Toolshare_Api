846ab5191a14024ba49bddc83644dfc56fefc66e
lakhvirsingh502 <lakhvirheer098@gmail.com>
2026-09-07 23:47:31 -0400

    modify new files

--- app/redis_client.py
+++ app/redis_client.py
@@ -1,7 +1,6 @@
 import redis.asyncio as redis
-
-redis_client = redis.Redis(
-    host="localhost",
-    port=6379,
-    decode_responses=True
-)
+import os
+from dotenv import load_dotenv
+load_dotenv()
+REDIS_URL = os.getenv("REDIS_URL")
+redis_client = redis.Redis.from_url(REDIS_URL)
